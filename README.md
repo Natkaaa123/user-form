85a1b59a838f6538e8b0b164732e5d1bb3d15ae5
nataliatopczewska <01178699@pw.edu.pl>
2024-05-31 20:18:40 +0200

    New files

---
+++ README.md
@@ -0,0 +1,3 @@
+# PIU24L_Topczewska_325329 NATALIA TOPCZEWSKA
+
+Repozytorium przechowuje pliki związane z pracą na ćwiczeniach z przedmiotu PIU w semestrze 24L. 
---
+++ src/user.form/Main.java
@@ -0,0 +1,14 @@
+package user.form;
+
+import java.text.ParseException;
+
+/**
+ * Klasa inicjująca działanie programu.
+ *
+ * @author Natalia Topczewska
+ */
+public class Main {
+    public static void main(String[] args) throws ParseException {GUI.createGUI();
+    }
+}
+
